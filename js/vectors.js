dc92b8b454a24ff4a8a635e2843c4889c199404f
Josh Murr <joshuadavidmurr@gmail.com>
2019-03-07 22:39:52 +0000

    gitignoring xcode stuff

--- js/vectors.js
+++ js/vectors.js
@@ -3,7 +3,7 @@ window.onload = function(){
         context = canvas.getContext("2d"),
         width = canvas.width = window.innerWidth,
         height = canvas.height = window.innerHeight,
-        centre, mouse, origin, dir;
+        centre, mouse, origin, dir, text, angle;
 
     //CLASS SYNTAX:
     class PVector {
@@ -49,24 +49,39 @@ window.onload = function(){
             t.sub(v);
             return t;
         }
+        getavg(v){
+            var t = new PVector(this.x, this.y);
+            t.add(v);
+            t.div(2);
+            return t;
+        }
+        avg(v){
+            this.x = (this.x + v.x)/2;
+            this.y = (this.y + v.x)/2;
+        }
+        getangle(){
+            return Math.atan2(this.y,this.x);
+        }
     }
     
     init();
     globalRender();
     
     function init() {
-        centre = new PVector(width/2, height/2);
+        centre = new PVector(300,200);
         mouse = new PVector(0,0);
         origin = new PVector(50,50);
+        text = new PVector(centre.x, centre.y);
+        text.avg(mouse);
     }
 
     function globalRender() {
 
 
         dir = mouse.getsub(centre);
-        dir.normalize();
-        dir.mult(50);
-
+        //dir.normalize();
+        //dir.mult(50);
+        
         
         //Animating:
         context.clearRect(0,0,width,height);
@@ -79,11 +94,46 @@ window.onload = function(){
         context.fillStyle = '#FF0000';
         context.fill();
 
-        context.beginPath();
         context.lineWidth = 2;
-        context.moveTo(50,50);
-        context.lineTo(origin.x + dir.x, origin.y + dir.y);
+        //(0,0) to mouse
+        context.beginPath();
+        context.strokeStyle = "red";
+        context.moveTo(0,0);
+        context.lineTo(mouse.x, mouse.y);
         context.stroke();
+
+        //0,0 to centre
+        context.beginPath();
+        context.strokeStyle = "blue";
+        context.moveTo(0,0);
+        context.lineTo(centre.x, centre.y);
+        context.stroke();
+
+        
+        //dir
+        context.beginPath();
+        context.strokeStyle = "black";
+        context.moveTo(centre.x,centre.y);
+        context.lineTo(centre.x + dir.x, centre.y + dir.y);
+
+        angle = -dir.getangle();
+        console.log(angle);
+        
+        context.save();
+        //Matrix transormation to draw arrow head:
+        context.transform(Math.cos(angle), -Math.sin(angle), Math.sin(angle), Math.cos(angle), centre.x + dir.x, centre.y + dir.y);
+
+        context.moveTo(0,0);
+        context.lineTo(-15, 15);
+        context.moveTo(0,0);
+        context.lineTo(-15, -15);
+
+        context.restore();
+        context.stroke();
+
+        context.font = "10px Monospace";
+        context.fillText("dir.x:"+dir.x+" dir.y:"+dir.y, text.x , text.y);
+        
 
 
         requestAnimationFrame(globalRender);
@@ -91,7 +141,13 @@ window.onload = function(){
     document.body.addEventListener("mousemove",function(event){
         mouse.x = event.clientX;
         mouse.y = event.clientY;
+        text = centre.getavg(mouse);
     });
+    document.body.addEventListener("mousedown",function(event){
+        origin.x = event.clientX;
+        origin.y = event.clientY;
+    });
+
 
 }
 
